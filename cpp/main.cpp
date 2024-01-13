cb6f02a31b78fff569430d7470387ba1867188d7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-01-13 16:30:14 +0000

    reduce size of measurement structure

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,14 +9,20 @@
 #include <utility>
 #include <vector>
 
+#include <marl/defer.h>
+#include <marl/event.h>
+#include <marl/scheduler.h>
+#include <marl/ticket.h>
+#include <marl/waitgroup.h>
+
 struct measurement_t {
-  double min;
-  double max;
+  float min;
+  float max;
   double total;
-  int64_t count;
+  double count;
 };
 
-std::pair<std::string_view, std::string_view> split_by(
+static std::pair<std::string_view, std::string_view> split_by(
   std::string_view string_to_split, std::string_view separator) {
   std::pair<std::string_view, std::string_view> split_strs;
   for (auto separator_position = string_to_split.find(separator);
@@ -32,18 +38,16 @@ std::pair<std::string_view, std::string_view> split_by(
   return split_strs;
 }
 
-size_t process_chunk(
+static size_t process_chunk(
   std::span<const char> buffer,
   std::map<std::string, measurement_t, std::less<>>& stations) {
   std::string_view data(buffer.data(), buffer.size());
   for (auto next = data.find('\n'); next != std::string_view::npos;
        next = data.find('\n')) {
     const auto line = data.substr(0, next);
-
     constexpr std::string_view separator = ";";
     const auto station_temp = split_by(line, separator);
-
-    double temperature = std::stod(std::string(station_temp.second));
+    float temperature = std::stof(std::string(station_temp.second));
     if (auto it = stations.find(station_temp.first); it != stations.end()) {
       it->second.min = std::min(it->second.min, temperature);
       it->second.max = std::max(it->second.max, temperature);
@@ -54,7 +58,6 @@ size_t process_chunk(
         {std::string(station_temp.first),
          {temperature, temperature, temperature, 1}});
     }
-
     // move to next line
     data.remove_prefix(next + 1);
   }
@@ -73,13 +76,13 @@ int main() {
   std::vector<char> buffer(buffer_size);
 
   size_t offset = 0;
-  std::map<std::string, measurement_t, std::less<>> stations;
+  std::map<std::string, measurement_t, std::less<>> all_stations;
   while (file) {
     file.read(buffer.data() + offset, buffer.size() - offset);
     std::streamsize bytes_read_count = file.gcount();
     // resize buffer to actual data size
     buffer.resize(bytes_read_count + offset);
-    offset = process_chunk(buffer, stations);
+    offset = process_chunk(buffer, all_stations);
     std::copy(buffer.end() - offset, buffer.end(), buffer.begin());
     // resize buffer back to original size for next read
     buffer.resize(buffer_size);
@@ -89,11 +92,13 @@ int main() {
     return std::copysign(std::round(std::abs(value) * 10) / 10, value);
   };
 
+  // output
+
   std::cout << std::fixed;
   std::cout << std::setprecision(1);
   std::cout << "{";
-  auto it = stations.begin();
-  for (; it != std::prev(stations.end()); ++it) {
+  auto it = all_stations.begin();
+  for (; it != std::prev(all_stations.end()); ++it) {
     const auto& [station, measurement] = *it;
     std::cout << station << '=' << measurement.min << '/'
               << rounded(measurement.total / (double)measurement.count) << '/'
